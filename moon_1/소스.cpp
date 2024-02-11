827d9cff28e43e3551f4fdd4132c67979a6d7830
Moonjeong Kim <moooon319@naver.com>
2024-02-12 02:25:36 +0900

    헬로

--- moon_1/소스.cpp
+++ moon_1/소스.cpp
@@ -1,46 +1,11 @@
 #include <iostream>
-#include <stack>
-#include <string>
+
 using namespace std;
 
 int main()
 {
-	int n,num;
-	string order;
-	stack<int> st;
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> order;
-
-		if (order == "push") {
-			cin >> num;
-			st.push(num);
-		}
-
-		else if (order == "pop") {
-			if (st.empty()) cout << "-1" << '\n';		
-			else {
-				cout << st.top() << '\n';
-				st.pop();
-			}
-		}
-
-		else if (order == "size") {
-			cout << st.size() << '\n';
-		}
-
-		else if (order == "empty") {
-			if (st.empty()) cout << "1" << '\n';
-			else cout << "0" << '\n';
-		}
-
-		else if (order == "top") {
-			if (st.empty()) cout << "-1" << '\n';
-			else cout << st.top() << '\n';
-		}
-	}
-	return 0;
+	cout << "Hello World!";
 }
+
 
 	
